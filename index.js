2b6b06413e99d3c63c8380b36b0b9c68d9f10db1
Miguel Ángel Reyes Moreno <miguelreyesmoreno@hotmail.com>
2023-03-02 12:23:14 -0600

    deleted whitelist

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ const port = process.env.PORT || 3050;
 
 app.use(express.json()) //* middleware
 
-const whiteList = ['http://127.0.0.1:5500', `http://localhost:${port}` ,'https://myapp.com', 'https://api-rest-expressjs-production.up.railway.app/']
+/* const whiteList = ['http://127.0.0.1:5500', `http://localhost:${port}` ,'https://myapp.com', 'https://api-rest-expressjs-production.up.railway.app/']
 const options = {
   origin: (origin, callback) => {
     whiteList.includes(origin || !origin) ? callback(null, true) : callback(new Error('No permitido'))
   }
-}
+} */
 
-app.use(cors(options))
+app.use(cors)
 
 app.get('/', (req, res) => {
   res.send('Hello world from Express.js!');
